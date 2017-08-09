9f0111ff951f3bc79849433a4731a9467db09266
Thierry <tom.kahl@hotmail.fr>
2017-08-09 17:09:04 +0000

    #1 added performance test for combining images

--- test.py
+++ test.py
@@ -1,22 +1,27 @@
 import unittest
 
 import PIL
+import timeit
 from PIL import Image
 from libs.pb_image import *
 
 imgPath = './dataTest'
 
+def test_combineNormal():
+	background = Image.open(imgPath + '/black.jpg')
+	image1 = Image.open(imgPath + '/white.jpg')
+	image2 = Image.open(imgPath + '/red.jpg')
+	image3 = Image.open(imgPath + '/blue.jpg')
+	image4 = Image.open(imgPath + '/green.jpg')
+	offset1 = 191
+	offset2 = 537
+	combined_image = pb_combineImages(background, image1, image2, image3, image4, offset1, offset2)
+	combined_image.save(imgPath + '/combined_testResult.jpg', 'JPEG', quality=100)
+
+
 class TestCombineImages(unittest.TestCase):
-	def test_combineNormal(self):
-		background = Image.open(imgPath + '/black.jpg')
-		image1 = Image.open(imgPath + '/white.jpg')		
-		image2 = Image.open(imgPath + '/red.jpg')
-		image3 = Image.open(imgPath + '/blue.jpg')
-		image4 = Image.open(imgPath + '/green.jpg')
-		offset1 = 191
-		offset2 = 537
-		combined_image = pb_combineImages(background, image1, image2, image3, image4, offset1, offset2)
-		combined_image.save(imgPath + '/combined_testResult.jpg', 'JPEG', quality=100)
+	def test_combineNormal_timed(self):
+		self.assertLess(timeit.timeit(test_combineNormal, number=1), 8)
 
 if __name__ == '__main__':
 	unittest.main()
